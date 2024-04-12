a576fce90c86a11c6dcf6287b0fbe73ea76c770d
Quang Mai <mai.quang@pdax.ph>
2024-04-12 15:30:17 +0700

    feat: add neovim multi cursor

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Delete current word and go to insert mode
+keymap.set("n", "<leader>i", "ciw", opts)
+
+-- Append mode
+keymap.set("n", "<leader>a", "bi", opts)
---
+++ lua/plugins/vscode-multi-cursor.lua
@@ -0,0 +1,17 @@
+return {
+  "vscode-neovim/vscode-multi-cursor.nvim",
+  event = "VeryLazy",
+  vscode = true,
+  opts = function(_, opts)
+    vim.keymap.set("n", "<C-n>", "mciw*<Cmd>nohl<CR>", {
+      remap = true,
+    })
+
+    local custom_opts = {
+      no_selection = true,
+    }
+
+    opts = vim.tbl_extend("force", opts, custom_opts)
+    return opts
+  end,
+}
